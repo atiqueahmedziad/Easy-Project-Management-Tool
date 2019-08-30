25e0db2cd686974ca25b05579196fd2cb0eaa120
Atique Ahmed Ziad <softfilebd@gmail.com>
2019-08-30 23:54:18 +0600

    Fixes #15 multiple user role authentication  (#32)
    * fixes #15 multiple user role authentication

--- src/App/LoginController.java
+++ src/App/LoginController.java
@@ -7,6 +7,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
+
+import App.IntroPage.IntropageController;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
@@ -17,6 +19,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.stage.Stage;
 
 public class LoginController implements Initializable {
@@ -32,25 +35,39 @@ public class LoginController implements Initializable {
     @FXML
     private Label isConnected;
 
+    public RadioButton adminToggle;
+    public RadioButton employeeToggle;
+
+    private String userRole;
+
+    public String getUserRole() {
+        return userRole;
+    }
+
     @FXML
     private void Login(ActionEvent actionEvent) throws IOException {
         Connect connect = new Connect();
         Connection connection=connect.getConnection();
 
+        if(!employeeToggle.isSelected() && !adminToggle.isSelected()){
+            isConnected.setText("Select Admin or Employee from above");
+            return;
+        }
+
         try {
-            Statement statement=connection.createStatement();
-            String sql="SELECT * FROM ADMIN WHERE username = '"+username.getText()+"' AND password = '"+password.getText()+"';";
-            ResultSet resultSet=statement.executeQuery(sql);
+            Statement statement = connection.createStatement();
+            String sql = "SELECT * FROM "+userRole+" WHERE username = '" + username.getText() + "' AND password = '" + password.getText() + "';";
+            ResultSet resultSet = statement.executeQuery(sql);
 
             // Resultset contains all the username and password
             // if the username and password from database matches only then intropage loads
-            if (resultSet.next()){
+            if (resultSet.next()) {
                 stage = (Stage) loginbutton.getScene().getWindow();
                 root = FXMLLoader.load(getClass().getResource("IntroPage/intropage.fxml"));
                 Scene scene = new Scene(root);
                 stage.setScene(scene);
                 stage.show();
-            }else {
+            } else {
                 // If username & password doesn't match
                 isConnected.setText("Username / password is wrong!");
             }
@@ -64,5 +81,19 @@ public class LoginController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
+    }
+
+    public void adminToggleOnAction(ActionEvent event) {
+        if(adminToggle.isSelected()){
+            employeeToggle.setSelected(false);
+            userRole = "ADMIN_AUTH";
+        }
+    }
+
+    public void employeeToggleOnAction(ActionEvent event) {
+        if(employeeToggle.isSelected()){
+            adminToggle.setSelected(false);
+            userRole = "EMPLOYEE_AUTH";
+        }
     }
 }
